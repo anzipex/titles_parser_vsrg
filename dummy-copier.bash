1e2f7d6af3b8f0a509db1fdce5244ad363169747
Anzipex <anzipex@gmail.com>
2018-12-17 12:40:58 +0300

    Update dummy-copier.bash
    Fixed copying; specialized game regions

--- dummy-copier.bash
+++ dummy-copier.bash
@@ -8,7 +8,7 @@ declare -a list_not_found
 if [ "$#" -ne 2 ]; then
     echo "arguments: titles_file roms_dir"
     echo "   titles_file - path to text file titles-*.txt"
-    echo "   roms_dir  - path to directory with ROMs (only sorted by GoodTools)"
+    echo "   roms_dir  - path to directory with ROMs (recommended by GoodTools)"
     exit 1
 fi
 
@@ -17,12 +17,26 @@ IFS=$'\n' read -d '' -r -a LIST_GAMES < $TITLES_FILE
 mkdir -p "$ROMS_DIR/!Selected"
 
 find_roms() {
-    if [[ -n $(find $ROMS_DIR -type f -name "$@*(*).*" -exec cp {} $ROMS_DIR/!Selected/ \; | sort -r | head -n 1) ]] &>/dev/null
-    then
-        echo "Found: $@"
-    else
-        list_not_found+=("$@")
-    fi
+    find $ROMS_DIR -maxdepth 1 -type f -name "*$@*(W).*" -exec cp {} "$ROMS_DIR/!Selected" \;
+    find $ROMS_DIR -maxdepth 1 -type f -name "*$@*(U).*" -exec cp {} "$ROMS_DIR/!Selected" \;
+    find $ROMS_DIR -maxdepth 1 -type f -name "*$@*(J).*" -exec cp {} "$ROMS_DIR/!Selected" \;
+    find $ROMS_DIR -maxdepth 1 -type f -name "*$@*(E).*" -exec cp {} "$ROMS_DIR/!Selected" \;
+    find $ROMS_DIR -maxdepth 1 -type f -name "*$@*(W) [*!*].*" -exec cp {} "$ROMS_DIR/!Selected" \;
+    find $ROMS_DIR -maxdepth 1 -type f -name "*$@*(U) [*!*].*" -exec cp {} "$ROMS_DIR/!Selected" \;
+    find $ROMS_DIR -maxdepth 1 -type f -name "*$@*(J) [*!*].*" -exec cp {} "$ROMS_DIR/!Selected" \;
+    find $ROMS_DIR -maxdepth 1 -type f -name "*$@*(E) [*!*].*" -exec cp {} "$ROMS_DIR/!Selected" \;
+    result=$(find $ROMS_DIR -maxdepth 1 -type f -name "$@*(*)*.*")
+    
+    for i in "${!result[@]}"
+    do
+        if [[ -z "${result[$i]}" ]];
+        then
+            list_not_found+=("$@")
+            not_found=$((not_found+1))
+        else
+            found=$((found+1))
+        fi
+    done
 }
 
 for rom in "${!LIST_GAMES[@]}"
@@ -35,11 +49,9 @@ do
     find_roms "$part_before"
 done
 
-echo "---"
-
-for i in "${list_not_found[@]}"
-do
-    echo "NOT found: $i"
-done
+echo "Not found: $not_found titles"
+echo ""
+echo "List not found:"
+printf '%s\n' "${list_not_found[@]}"
 
 exit 0
